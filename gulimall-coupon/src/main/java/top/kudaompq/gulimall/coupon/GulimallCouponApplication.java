95886d86806e5b1e08e3e25b5c9042e3e3eaea58
kudaompq <kudaompq@foxmail.com>
2024-01-20 17:47:56 +0800

    添加cloud alibaba依赖管理，引入nacos

--- gulimall-coupon/src/main/java/top/kudaompq/gulimall/coupon/GulimallCouponApplication.java
+++ gulimall-coupon/src/main/java/top/kudaompq/gulimall/coupon/GulimallCouponApplication.java
@@ -3,8 +3,10 @@ package top.kudaompq.gulimall.coupon;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @MapperScan("top.kudaompq.gulimall.coupon.dao")
+@EnableDiscoveryClient // 开启服务注册发现功能
 @SpringBootApplication
 public class GulimallCouponApplication {
 
